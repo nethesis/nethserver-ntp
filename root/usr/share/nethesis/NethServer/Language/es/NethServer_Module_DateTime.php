9924843736575ffaecd6d0d9bc1486d7ca7ed2f2
Ana Fernandez <anuska_716@hotmail.com>
2014-05-06 09:57:47 +0200

    Web interface: add Spanish translation. Refs #2732

---
+++ root/usr/share/nethesis/NethServer/Language/es/NethServer_Module_DateTime.php
@@ -0,0 +1,13 @@
+<?php 
+
+$L['DateTime_header'] = 'Cambiar la fecha y hora actuales (${0})'; 
+$L['DateTime_Description'] = 'Configurar fecha y hora del sistema'; 
+$L['DateTime_Tags'] = 'hora minuto zona horaria fecha tiempo ntp'; 
+$L['DateTime_Title'] = 'Fecha y hora'; 
+$L['date_label'] = 'Fecha'; 
+$L['server_label'] = 'Servidor NTP'; 
+$L['Status_disabled_label'] = 'Ajuste manual'; 
+$L['Status_enabled_label'] = 'Sincronización de red'; 
+$L['time_label'] = 'Horas y minutos'; 
+$L['timezone_label'] = 'Zona horaria'; 
+
